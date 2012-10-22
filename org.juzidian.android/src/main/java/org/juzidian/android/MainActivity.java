baa5417c1b6a6632497b96aee7e0fa84bc5c131b
Nathan Jones <nathan@ncjones.com>
2012-10-22 21:09:14 +1300

    Automatically generate dictionary DB file for Android app.

--- org.juzidian.android/src/main/java/org/juzidian/android/MainActivity.java
+++ org.juzidian.android/src/main/java/org/juzidian/android/MainActivity.java
@@ -1,5 +1,10 @@
 package org.juzidian.android;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -27,21 +32,50 @@ import com.j256.ormlite.support.ConnectionSource;
 
 public class MainActivity extends Activity {
 
+	private static final String DICTIONARY_DB_PATH = "/data/data/org.juzidian.android/juzidian-dictionary.db";
+
 	private Dictionary dictionary;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
+		this.initializeDbFile();
 		final Injector injector = Guice.createInjector(new DictionaryModule() {
 			@Override
 			protected ConnectionSource createConnectionSource(final String jdbcUrl) throws SQLException {
-				final SQLiteDatabase sqliteDb = SQLiteDatabase.openDatabase("/data/data/org.juzidian.android/juzidian.db", null,
+				final SQLiteDatabase sqliteDb = SQLiteDatabase.openDatabase(DICTIONARY_DB_PATH, null,
 						SQLiteDatabase.OPEN_READONLY | SQLiteDatabase.NO_LOCALIZED_COLLATORS);
 				return new AndroidConnectionSource(sqliteDb);
 			}
 		});
 		this.dictionary = injector.getInstance(Dictionary.class);
+	}
+
+	private void initializeDbFile() {
+		final InputStream inputStream = this.getDictionaryDbInputStream();
+		final File dbFile = new File(DICTIONARY_DB_PATH);
+		dbFile.delete();
+		try {
+			dbFile.createNewFile();
+			this.copy(inputStream, new FileOutputStream(dbFile));
+		} catch (final IOException e) {
+			throw new RuntimeException("Failed to initialize DB file.");
+		}
+	}
+
+	private InputStream getDictionaryDbInputStream() {
+		return this.getResources().openRawResource(R.raw.juzidian_dictionary);
+	}
+
+	public void copy(final InputStream in, final OutputStream out) throws IOException {
+		final byte[] buf = new byte[10000];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
 	}
 
 	@Override
