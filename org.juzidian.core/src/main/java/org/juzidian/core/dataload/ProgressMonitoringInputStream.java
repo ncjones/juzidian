aac37857be2a18caef0f8a53cb96c5f8551513f7
Nathan Jones <nathan@ncjones.com>
2013-02-24 13:49:37 +1300

    Create input stream wrapper for monitoring download progress.

---
+++ org.juzidian.core/src/main/java/org/juzidian/core/dataload/ProgressMonitoringInputStream.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright Nathan Jones 2013
+ *
+ * This file is part of Juzidian.
+ *
+ * Juzidian is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Juzidian is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Juzidian.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.juzidian.core.dataload;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * An input stream wrapper which notifies a {@link DownloadProgressHandler} when
+ * ever some number of bytes have been read.
+ */
+class ProgressMonitoringInputStream extends InputStream {
+
+	private static final int NOTIFICATION_INCREMENT_SIZE = 1 << 13;
+
+	private final InputStream delegate;
+
+	private final DownloadProgressHandler handler;
+
+	private final int contentLength;
+
+	private int bytesRead = 0;
+
+	public ProgressMonitoringInputStream(final InputStream delegate, final int contentLength, final DownloadProgressHandler handler) {
+		this.delegate = delegate;
+		this.contentLength = contentLength;
+		this.handler = handler;
+	}
+
+	@Override
+	public int read() throws IOException {
+		final int nextByte = this.delegate.read();
+		if (nextByte != -1) {
+			this.bytesRead += 1;
+		}
+		if (this.bytesRead % NOTIFICATION_INCREMENT_SIZE == 0 || nextByte == -1) {
+			this.handler.handleProgress(this.contentLength, this.bytesRead);
+			this.bytesRead += 0;
+		}
+		return nextByte;
+	}
+
+}
---
+++ org.juzidian.core/src/test/java/org/juzidian/core/dataload/ProgressMonitoringInputStreamTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright Nathan Jones 2013
+ *
+ * This file is part of Juzidian.
+ *
+ * Juzidian is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Juzidian is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Juzidian.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.juzidian.core.dataload;
+
+import static org.mockito.Matchers.eq;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ProgressMonitoringInputStreamTest {
+
+	@Test
+	public void downloadProgressHandlerShouldRecieveNotifications() throws Exception {
+		final DownloadProgressHandler handler = Mockito.mock(DownloadProgressHandler.class);
+		final ByteArrayInputStream bais = new ByteArrayInputStream(new byte[18000]);
+		final ProgressMonitoringInputStream inputStream = new ProgressMonitoringInputStream(bais, 18000, handler);
+		consumeInputStream(inputStream);
+		Mockito.verify(handler).handleProgress(eq(18000), eq(8192));
+		Mockito.verify(handler).handleProgress(eq(18000), eq(16384));
+		Mockito.verify(handler).handleProgress(eq(18000), eq(18000));
+	}
+
+	private static void consumeInputStream(final InputStream inputStream) throws IOException {
+		while (inputStream.read() != -1) {
+			/* discard bytes */
+		}
+	}
+
+}
