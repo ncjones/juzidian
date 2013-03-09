255341fc6608177cef6269ce4f04f56b39f96827
Nathan Jones <nathan@ncjones.com>
2013-03-09 20:32:01 +0800

    Implement automatic data provisioning in CLI.

--- org.juzidian.cli/src/main/java/org/juzidian/cli/JuzidianCli.java
+++ org.juzidian.cli/src/main/java/org/juzidian/cli/JuzidianCli.java
@@ -18,6 +18,8 @@
  */
 package org.juzidian.cli;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.sql.SQLException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -27,6 +29,13 @@ import org.juzidian.core.Dictionary;
 import org.juzidian.core.DictionaryEntry;
 import org.juzidian.core.SearchQuery;
 import org.juzidian.core.SearchType;
+import org.juzidian.core.dataload.DictionaryResource;
+import org.juzidian.core.dataload.DictionaryResourceDownloader;
+import org.juzidian.core.dataload.DictionaryResourceDownloaderException;
+import org.juzidian.core.dataload.DictionaryResourceRegistry;
+import org.juzidian.core.dataload.DictionaryResourceRegistryService;
+import org.juzidian.core.dataload.DictonaryResourceRegistryServiceException;
+import org.juzidian.core.datastore.DbDictionaryDataStore;
 import org.juzidian.core.inject.DictionaryModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,28 +54,28 @@ public class JuzidianCli {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(JuzidianCli.class);
 
-	public static void main(final String[] args) {
-		if (args.length < 1) {
-			System.out.println("Dictionary DB file must be specified.");
-			return;
+	private static final File DICTIONARY_DB_FILE = new File(System.getProperty("user.home") + "/.juzidian/juzidian-dictionary.db");
+
+	private static Injector INJECTOR = Guice.createInjector(new DictionaryModule() {
+
+		@Override
+		protected ConnectionSource createConnectionSource() throws SQLException {
+			return new JdbcConnectionSource("jdbc:sqlite:" + DICTIONARY_DB_FILE.getAbsolutePath());
 		}
-		if (args.length < 2) {
+	});
+
+	public static void main(final String[] args) throws Exception {
+		if (args.length < 1) {
 			System.out.println("Search type must be specified.");
 			return;
 		}
-		if (args.length < 3) {
+		if (args.length < 2) {
 			System.out.println("Search query must be specified.");
 			return;
 		}
-		final String dictionaryDbFilePath = args[0];
 		final SearchType searchType = SearchType.valueOf(args[1]);
-		final Injector injector = Guice.createInjector(new DictionaryModule() {
-			@Override
-			protected ConnectionSource createConnectionSource() throws SQLException {
-				return new JdbcConnectionSource("jdbc:sqlite:" + dictionaryDbFilePath);
-			}
-		});
-		final Dictionary dictionary = injector.getInstance(Dictionary.class);
+		initializeDb();
+		final Dictionary dictionary = INJECTOR.getInstance(Dictionary.class);
 		final Runtime runtime = Runtime.getRuntime();
 		final long totalMemory = runtime.totalMemory();
 		final long freeMemory = runtime.freeMemory();
@@ -74,6 +83,49 @@ public class JuzidianCli {
 		final String queryString = args[2];
 		final List<DictionaryEntry> foundCharacters = findAllWords(dictionary, queryString, searchType);
 		printSearchResults(foundCharacters);
+	}
+
+	private static void initializeDb() throws Exception {
+		if (!DICTIONARY_DB_FILE.exists()) {
+			LOGGER.info("Dictionary DB missing.");
+			downloadDb(true);
+		} else {
+			final DbDictionaryDataStore dataStore = INJECTOR.getInstance(DbDictionaryDataStore.class);
+			if (dataStore.getCurrentDataFormatVersion() != DbDictionaryDataStore.DATA_FORMAT_VERSION) {
+				LOGGER.info("Dictionary DB incompatible.");
+				downloadDb(false);
+			} else {
+				LOGGER.debug("Dictionary DB compatible.");
+			}
+		}
+	}
+
+	private static void downloadDb(final boolean createNewFile) throws Exception {
+		DICTIONARY_DB_FILE.getParentFile().mkdirs();
+		final DictionaryResource resource = getDictionaryResource();
+		final DictionaryResourceDownloader downloader = INJECTOR.getInstance(DictionaryResourceDownloader.class);
+		LOGGER.info("Downloading dictionary DB: " + resource.getUrl());
+		try {
+			downloader.download(resource, new FileOutputStream(DICTIONARY_DB_FILE), new DownloadProgressLogger());
+		} catch (final DictionaryResourceDownloaderException e) {
+			if (createNewFile) {
+				/* make sure the newly created (empty) file is removed */
+				DICTIONARY_DB_FILE.delete();
+			}
+			throw e;
+		}
+	}
+
+	private static DictionaryResource getDictionaryResource() throws DictonaryResourceRegistryServiceException {
+		final DictionaryResourceRegistry registry = getDictionaryResourceRegistry();
+		return registry.getDictionaryResources().get(0);
+	}
+
+	private static DictionaryResourceRegistry getDictionaryResourceRegistry() throws DictonaryResourceRegistryServiceException {
+		final DictionaryResourceRegistryService registryService = INJECTOR.getInstance(DictionaryResourceRegistryService.class);
+		LOGGER.debug("Getting registry of available dictionary DBs.");
+		final DictionaryResourceRegistry registry = registryService.getDictionaryResourceRegistry(DbDictionaryDataStore.DATA_FORMAT_VERSION);
+		return registry;
 	}
 
 	private static List<DictionaryEntry> findAllWords(final Dictionary dictionary, final String queryString, final SearchType searchType) {
